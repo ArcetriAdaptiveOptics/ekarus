17178a0ad97a8ceede55040ff959f985730ce30d
menessao <menessinimatteo@gmail.com>
2025-11-21 15:18:29 +0100

    Added linearDarkFieldControl notebook

--- ekarus/mains/main251020_optical_gains.py
+++ ekarus/mains/main251020_optical_gains.py
@@ -10,17 +10,21 @@ from ekarus.e2e.utils import my_fits_package as myfits
 
 def main(tn:str='optical_gains', 
          optimize_gain:bool=False,
-         gain_list = None):
+         gain_list = None,
+         atmo_tn='ekarus_3.5cm',
+         N:int=1):
+    
+    if gain_list is not None:
+        optimize_gain = True
     
     if optimize_gain is True and gain_list is None:
         gain_list = xp.arange(1,10)/10
         gain_list = gain_list.tolist()
 
     ssao = SingleStageAO(tn)
-    ssao.initialize_turbulence()
+    ssao.initialize_turbulence(atmo_tn)
     KL, m2c = ssao.define_KL_modes(ssao.dm, zern_modes=2)
-    r0 = (1/xp.sum(ssao.atmo_pars['r0']**(-5/3)))**(3/5)
-    saveprefix = f'mod{ssao.sc.modulationAngleInLambdaOverD:1.0f}_r0={r0*1e+2:1.0f}cm_'
+    saveprefix = f'mod{ssao.sc.modulationAngleInLambdaOverD:1.0f}_'
     amp = 50e-9
     _, IM = ssao.compute_reconstructor(ssao.sc, KL, ssao.pyr.lambdaInM, ampsInM=amp, save_prefix=saveprefix)
     ssao.sc.load_reconstructor(IM,m2c)
@@ -28,17 +32,15 @@ def main(tn:str='optical_gains',
 
     lambdaRef = ssao.pyr.lambdaInM
 
-    try:
-        ma_res_screens = myfits.read_fits(os.path.join(ssao.savepath,'residual_phases.fits'))
-    except FileNotFoundError:
-        sig2, input_sig2 = ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='')
-        ma_res_screens = myfits.read_fits(os.path.join(ssao.savepath,'residual_phases.fits'))
+    # try:
+    #     ma_res_screens = myfits.read_fits(os.path.join(ssao.savepath,'residual_phases.fits'))
+    # except FileNotFoundError:
+    sig2, input_sig2 = ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='')
+    ma_res_screens = myfits.read_fits(os.path.join(ssao.savepath,'residual_phases.fits'))
 
-    N = 4
     offset = 100
     loop_residual_phases = xp.zeros([N,int(xp.sum(1-ssao.cmask))])
     step = (xp.shape(xp.asarray(ma_res_screens.data))[0]-offset)//N
-    print(step)
     for j in range(N):
         loop_residual_phases[j,:] = xp.asarray(ma_res_screens[j*step+offset].data[~ma_res_screens[j*step+offset].mask])
 
@@ -46,9 +48,7 @@ def main(tn:str='optical_gains',
     cl_opt_gains, pl_opt_gains = ssao.calibrate_optical_gains_from_precorrected_screens(
                                  loop_residual_phases,
                                  slope_computer=ssao.sc, MM=KL, 
-                                 save_prefix = saveprefix, amps=amp)
-    # cl_opt_gains, pl_opt_gains = ssao.calibrate_optical_gains(N=3, slope_computer=ssao.sc, MM=KL, 
-    #                              save_prefix = saveprefix, amps=amp)
+                                 save_prefix = saveprefix, ampsInM=amp)
     plt.figure()
     # plt.plot(xp.asnumpy(ol_opt_gains),'-.',label='open loop')
     plt.plot(xp.asnumpy(cl_opt_gains),'-.',label='closed loop')
@@ -58,10 +58,9 @@ def main(tn:str='optical_gains',
     plt.grid()
 
     ogcl_ssao = SingleStageAO(tn)
-    ogcl_ssao.initialize_turbulence()
-    ogcl_ssao.pyr.set_modulation_angle(ssao.sc.modulationAngleInLambdaOverD)
+    ogcl_ssao.initialize_turbulence(atmo_tn)
     ogcl_ssao.sc.load_reconstructor(IM,m2c)
-    ogcl_ssao.sc.modalGains /= pl_opt_gains
+    ogcl_ssao.sc.modalGains /= cl_opt_gains
 
     # ogol_ssao = SingleStageAO(tn)
     # ogol_ssao.initialize_turbulence()
@@ -70,14 +69,12 @@ def main(tn:str='optical_gains',
     # ogol_ssao.sc.modalGains /= cl_opt_gains
     # ogol_ssao.sc.set_new_gain(gain)
 
-    # ogpl_ssao = SingleStageAO(tn)
-    # ogpl_ssao.initialize_turbulence()
-    # ogpl_ssao.pyr.set_modulation_angle(ssao.sc.modulationAngleInLambdaOverD)
-    # ogpl_ssao.sc.load_reconstructor(IM,m2c)
-    # ogpl_ssao.sc.modalGains /= pl_opt_gains
-    # ogpl_ssao.sc.set_new_gain(gain)
+    ogpl_ssao = SingleStageAO(tn)
+    ogpl_ssao.initialize_turbulence(atmo_tn)
+    ogpl_ssao.sc.load_reconstructor(IM,m2c)
+    ogpl_ssao.sc.modalGains /= pl_opt_gains
 
-    it_ss = 200
+    it_ss = max(200,int(ssao.Nits//2))
     if optimize_gain is True:
         gain_list = xp.array(gain_list)
         print('Finding best gains:')
@@ -85,11 +82,6 @@ def main(tn:str='optical_gains',
         SR_vec = xp.zeros(N)
         for jj in range(N):
             g = gain_list[jj]
-            ogcl_ssao = SingleStageAO(tn)
-            ogcl_ssao.initialize_turbulence()
-            ogcl_ssao.pyr.set_modulation_angle(ssao.sc.modulationAngleInLambdaOverD)
-            ogcl_ssao.sc.load_reconstructor(IM,m2c)
-            ogcl_ssao.sc.modalGains /= pl_opt_gains
             ogcl_ssao.sc.set_new_gain(g)
             err2, _ = ogcl_ssao.run_loop(lambdaRef, ssao.starMagnitude)
             SR = xp.mean(xp.exp(-err2[-it_ss:]))
@@ -98,11 +90,6 @@ def main(tn:str='optical_gains',
 
         best_gain = gain_list[xp.argmax(SR_vec)]
         print(f'Selecting best integrator gain: {best_gain:1.1f}, yielding SR={xp.max(SR_vec):1.2f} @{lambdaRef*1e+9:1.0f}[nm]')   
-        ogcl_ssao = SingleStageAO(tn)
-        ogcl_ssao.initialize_turbulence()
-        ogcl_ssao.pyr.set_modulation_angle(ssao.sc.modulationAngleInLambdaOverD)
-        ogcl_ssao.sc.load_reconstructor(IM,m2c)
-        ogcl_ssao.sc.modalGains /= pl_opt_gains
         ogcl_ssao.sc.set_new_gain(best_gain)
         if xp.on_gpu:
             gain_list = gain_list.get()
@@ -116,17 +103,17 @@ def main(tn:str='optical_gains',
 
     ogcl_ssao.KL = KL
     # ogol_ssao.KL = KL
-    # ogpl_ssao.KL = KL
+    ogpl_ssao.KL = KL
 
-    ogcl_sig2, _ = ogcl_ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='ogcl_')
+    ogcl_sig2, input_sig2 = ogcl_ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='ogcl_')
     # ogol_sig2, _ = ogol_ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='ogol_')
-    # ogpl_sig2, _ = ogpl_ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='ogpl_')
+    ogpl_sig2, _ = ogpl_ssao.run_loop(lambdaRef, ssao.starMagnitude, save_prefix='ogpl_')
 
     ssao.plot_iteration(lambdaRef, frame_id=-1, save_prefix='')
     # psd,pix_dist=ssao.plot_contrast(lambdaRef, frame_ids=xp.arange(ssao.Nits-100,ssao.Nits).tolist(), save_prefix='')
     ogcl_ssao.plot_iteration(lambdaRef, frame_id=-1, save_prefix='ogcl_')
     # ogol_ssao.plot_iteration(lambdaRef, frame_id=-1, save_prefix='ogol_')
-    # ogpl_ssao.plot_iteration(lambdaRef, frame_id=-1, save_prefix='ogpl_')
+    ogpl_ssao.plot_iteration(lambdaRef, frame_id=-1, save_prefix='ogpl_')
 
     # plt.figure()
     # plt.plot(xp.asnumpy(pix_dist),xp.asnumpy(psd),'--',label='no OG compensation')
@@ -143,7 +130,7 @@ def main(tn:str='optical_gains',
     plt.plot(xp.asnumpy(tvec),xp.asnumpy(sig2),'-.',label='no compensation')
     plt.plot(xp.asnumpy(tvec),xp.asnumpy(ogcl_sig2),'-.',label='OG (close loop) compensation')
     # plt.plot(xp.asnumpy(tvec),xp.asnumpy(ogol_sig2),'-.',label='OG (open loop) compensation')
-    # plt.plot(xp.asnumpy(tvec),xp.asnumpy(ogpl_sig2),'-.',label='OG (perfect loop) compensation')
+    plt.plot(xp.asnumpy(tvec),xp.asnumpy(ogpl_sig2),'-.',label='OG (perfect loop) compensation')
     plt.legend()
     plt.grid()
     plt.xlim([0.0,tvec[-1]])
